f157fda3740c3456900b8992c08e65d6fc248ed7
Prince Kumar <princekumarsingh15022000@gmail.com>
2021-02-17 19:56:32 +0530

    First React commit

--- src/index.js
+++ src/index.js
@@ -24,12 +24,13 @@ const Book=()=>{
     <BookList/>
     <Author/>
     <Price/>
+
   </div>
   );
 }
 
 const BookList = () =><img src='https://images-na.ssl-images-amazon.com/images/I/41EzNnr4YUL._AC_SX184_.jpg'/>
-const Author=()=><h1>Author Name</h1>
+const Author=()=><h1>Author has a Name</h1>
 const Price=()=><h4>$20</h4>
 
 ReactDOM.render(<Greeting/>,document.getElementById('root'));
